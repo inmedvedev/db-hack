615581384c56d7e31671a1c95de1eec1c50bcc79
turim0 <ivan_medvedev_1996@mail.ru>
2021-03-05 16:02:03 +0300

    pep8 standart

--- db_hack.py
+++ db_hack.py
@@ -1,20 +1,17 @@
 from datacenter.models import Schoolkid, Chastisement, Commendation, Lesson, Mark
-from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 import random
-import sys
 
 
 def get_schoolkid(full_name):
     try:
         child = Schoolkid.objects.get(full_name__contains=full_name)
-    except (MultipleObjectsReturned, ObjectDoesNotExist):
-        print("Учетная запись не найдена")
-        sys.exit()
-    return child
+        return child
+    except (Schoolkid.MultipleObjectsReturned, Schoolkid.DoesNotExist):
+        print (f"Учетная запись {full_name} не найдена, уточните имя")
 
 
 def fix_marks(child):
-    Mark.objects.filter(schoolkid=child, points__in = [2,3]).update(points = 4)
+    Mark.objects.filter(schoolkid=child, points__in=[2, 3]).update(points=4)
 
 
 def remove_chastisements(child):
@@ -22,6 +19,28 @@ def remove_chastisements(child):
 
 
 def create_commendation(child, subject):
-    lesson = random.choice(Lesson.objects.filter(year_of_study=6, group_letter="А", subject__title__contains=subject))
-    commendations =  ["Очень хороший ответ!","Талантливо!","Ты сегодня прыгнул выше головы!","Я поражен!","Уже существенно лучше!","Потрясающе!","Замечательно!","Прекрасное начало!","Так держать!","Ты на верном пути!"]
-    Commendation.objects.create(teacher=lesson.teacher, subject=lesson.subject, schoolkid=child, created=lesson.date, text=random.choice(commendations))
+    lessons = Lesson.objects.filter(
+        year_of_study=6,
+        group_letter="А",
+        subject__title__contains=subject
+    )
+    lesson = lessons.order_by('?').first()
+    commendations = [
+        "Очень хороший ответ!",
+        "Талантливо!",
+        "Ты сегодня прыгнул выше головы!",
+        "Я поражен!",
+        "Уже существенно лучше!",
+        "Потрясающе!",
+        "Замечательно!",
+        "Прекрасное начало!",
+        "Так держать!",
+        "Ты на верном пути!"
+    ]
+    Commendation.objects.create(
+        teacher=lesson.teacher,
+        subject=lesson.subject,
+        schoolkid=child,
+        created=lesson.date,
+        text=random.choice(commendations)
+    )
